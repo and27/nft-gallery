af8d645c2789e959faad7ad7d5df491a844e1e1d
Andrés Banda <andrezh_gln7@live.com>
2024-06-13 23:18:56 -0500

    refactor: replace a elements with Links in footer socials

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,18 +1,22 @@
+import Link from "next/link";
 import { FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa6";
 
 const Footer = () => {
   return (
     <footer className="bg-neutral-950 text-white py-10 text-center">
       <div className="flex gap-5 justify-center my-5">
-        <a href="#footer" className="text-white">
+        <Link href="#footer" className="text-white">
+          <span className="sr-only">Facebook</span>
           <FaFacebook size={20} />
-        </a>
-        <a href="#footer" className="text-white">
+        </Link>
+        <Link href="#footer" className="text-white">
+          <span className="sr-only">Twiter</span>
           <FaTwitter size={20} />
-        </a>
-        <a href="#footer" className="text-white">
+        </Link>
+        <Link href="#footer" className="text-white">
+          <span className="sr-only">Instagram</span>
           <FaInstagram size={20} />
-        </a>
+        </Link>
       </div>
       <p className="text-sm">
         &copy; NFT Gallery 2021-2025 All rights reserved
