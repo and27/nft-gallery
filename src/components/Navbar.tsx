d8672740758e68869070372d0e5203bb92aa9e85
Andrés Banda <andrezh_gln7@live.com>
2024-06-14 12:23:53 -0500

    feat: create mobile nav with menu and close icons

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,8 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 type NavType = {
   name: string;
@@ -25,19 +29,80 @@ const navItems: NavType[] = [
 ];
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const isMobile = typeof window !== "undefined" && window.innerWidth < 768;
+  const pathname = usePathname();
+  const isActive = (href: string) => {
+    return pathname === href ? "text-violet-500" : "";
+  };
+
+  const openMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <>
-      <nav className="flex justify-between items-center py-4 px-8 bg-neutral-950/10 backdrop-blur-lg backdrop-filter fixed w-full z-10">
-        <div className="text-white font-bold text-2xl">NFT</div>
-        <ul className="flex gap-8 text-white">
-          {navItems.map((item) => (
-            <li key={item.name}>
-              <Link href={item.link}>{item.name}</Link>
-            </li>
-          ))}
-        </ul>
-        <button className="px-4 py-2 border border-white text-white rounded-md">
-          Get started
+      <nav className="relative">
+        <div
+          className={`flex flex-col md:flex-row justify-between items-center py-4 px-8  bg-neutral-950/70 md:bg-neutral-950/10 backdrop-blur-lg backdrop-filter fixed w-full z-20
+          ${isMobile ? (isOpen ? "top-0 left-0 h-screen" : "hidden") : ""}
+            `}
+        >
+          <Link href="#" className="text-white font-bold text-2xl">
+            NFT
+          </Link>
+          <ul className="flex flex-col md:flex-row py-5 gap-8 text-white">
+            {navItems.map((item) => (
+              <li key={item.name}>
+                <Link href={item.link}>{item.name}</Link>
+              </li>
+            ))}
+          </ul>
+          <Link
+            href="#contact"
+            className="px-4 py-2 border border-white text-white rounded-md hover:bg-white hover:text-black"
+            onClick={openMenu}
+          >
+            Contact us
+          </Link>
+        </div>
+        <button
+          className="md:hidden absolute top-4 right-4 z-50"
+          aria-label="Open Menu"
+          title="Open Menu"
+          onClick={openMenu}
+        >
+          {isOpen ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6 text-white"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6 text-white"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M4 6h16M4 12h16m-7 6h7"
+              />
+            </svg>
+          )}
         </button>
       </nav>
     </>
