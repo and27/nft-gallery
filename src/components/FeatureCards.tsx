7fe3a13b998e90886101197d57a5c3b3405fdbb6
Andrés Banda <andrezh_gln7@live.com>
2024-06-14 00:57:49 -0500

    refactor: change how we render Icon component to fix ts error

--- src/components/FeatureCards.tsx
+++ src/components/FeatureCards.tsx
@@ -1,18 +1,12 @@
 import { IconType } from "react-icons";
-import { FaLock, FaUser, FaPaintBrush, FaRocket } from "react-icons/fa";
+import { FaShareAltSquare } from "react-icons/fa";
+import { FaEye, FaFolderOpen, FaLock } from "react-icons/fa6";
 
 type itemsType = {
   id: string;
   name: string;
   description: string;
-  icon: string;
-};
-
-const icons: Record<string, IconType> = {
-  FaPaintBrush: FaPaintBrush,
-  FaLock: FaLock,
-  FaUser: FaUser,
-  FaRocket: FaRocket,
+  icon: IconType;
 };
 
 const items: itemsType[] = [
@@ -21,44 +15,42 @@ const items: itemsType[] = [
     name: "Organization",
     description:
       "Arrange your NFTs into personalized collections and access them quickly.",
-    icon: "FaFolderOpen",
+    icon: FaFolderOpen,
   },
   {
     id: "2",
     name: "Visualization",
     description:
       "Enjoy a modern bookshelf view and see the details of each NFT with a single click.",
-    icon: "FaEye",
+    icon: FaEye,
   },
   {
     id: "3",
     name: "Sharing",
     description:
       "Share your collections and discover those of other users in our integrated social network.",
-    icon: "FaShareAlt",
+    icon: FaShareAltSquare,
   },
   {
     id: "4",
     name: "Security",
     description:
       "Protect your digital assets with our robust security measures.",
-    icon: "FaLock",
+    icon: FaLock,
   },
 ];
 
 const FeatureCards = () => (
-  <section className="text-center pb-[8rem] -mt-10 relative z-10">
+  <section className="text-center pb-[8rem] -mt-10 relative z-10 px-4">
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 max-w-[1280px] mx-auto p-10  rounded-xl bg-violet-950 ">
-      {items.map((item) => {
-        const iconName = item.icon;
-        const Icon = icons[iconName as keyof typeof icons];
+      {items.map(({ icon: Icon, ...item }) => {
         return (
           <div
             key={item.id}
             className="rounded-lg flex flex-col gap-4 text-white items-start text-left transition-all cursor-pointer mb-10 lg:mb-0 "
           >
             <div className="flex gap-5 items-center justify-start">
-              {/* <Icon size={"24"} className="text-white" /> */}
+              <Icon size={"24"} className="text-white" />
               <h2 className="text-2xl font-bold">{item.name}</h2>
               <div className="border border-white rounded-full p-1"></div>
             </div>
