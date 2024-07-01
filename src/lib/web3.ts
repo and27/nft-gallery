a70281cb4a0e179c43cf1977d13f0a2ca62aac3c
Andrés Banda <andrezh_gln7@live.com>
2024-07-01 01:13:40 -0500

    update infura api key

--- src/lib/web3.ts
+++ src/lib/web3.ts
@@ -2,12 +2,14 @@ import Web3 from "web3";
 
 let web3: Web3;
 
+const infuraKey = process.env.REACT_APP_INFURA_KEY;
+
 if (typeof window !== "undefined" && window.ethereum) {
   window.ethereum.request?.({ method: "eth_requestAccounts" });
   web3 = new Web3(window.ethereum as any);
 } else {
   const provider = new Web3.providers.HttpProvider(
-    "https://mainnet.infura.io/v3/YOUR_INFURA_PROJECT_ID"
+    `https://sepolia.infura.io/v3/${infuraKey}`
   );
   web3 = new Web3(provider);
 }
